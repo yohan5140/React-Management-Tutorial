249b493b10c8fb01949b4348f8c142f35202e09f
BuildTools <unconfigured@null.spigotmc.org>
2019-03-04 22:36:04 +0900

    a

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "indent": [
+            "warn",
+            2
+        ],
+        "semi" : [
+            "error",
+            "always"
+        ], 
+        "no-unused-vars": 1
+    }
+};
